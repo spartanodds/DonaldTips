47074565385110897110b333ff027282b3dfaae6
spartanodds <luks2015b@gmail.com>
2025-07-30 21:11:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -42,3 +42,15 @@ bot.on('message', async (msg) => {
     bot.sendMessage(chatId, response);
   }
 });
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("DonaldTips Bot ativo.");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
